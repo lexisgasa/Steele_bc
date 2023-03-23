7acd41e2c2ce36f5bbe30bc6c6c64ddbd5eba736
Alexis <lexisgasa3007@gmail.com>
2023-03-23 11:44:53 +0100

    comments updated

--- 03. Javascript/01. guessGame.js
+++ 03. Javascript/01. guessGame.js
@@ -1,3 +1,5 @@
+// este numero maximo es para saber cuantos numeros random generar
+
 let maxNumber = parseInt(prompt("Which will be the maximum number"))
 
 
@@ -7,6 +9,7 @@ console.log(randomNumber)
 let guess = prompt("Make a guess");
 let attempts = 1;
 
+//  while para preguntar hasta que se acierte, el promp genera string por lo tanto se necesita el parseint para pasarlo a numero, ya que queremos que la q sirva para salir
 while ( parseInt(guess) !== randomNumber ) {
     if (guess === "q" ) break;
     attempts++;
