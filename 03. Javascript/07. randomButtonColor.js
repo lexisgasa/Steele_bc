690cd639d3570ecb3b46b9631e0ca04ceda0e133
Alexis <lexisgasa3007@gmail.com>
2023-03-23 11:46:56 +0100

    comments updated

--- 03. Javascript/07. randomButtonColor.js
+++ 03. Javascript/07. randomButtonColor.js
@@ -3,12 +3,15 @@ title.innerText = "Button random color changer"
 document.body.append(title) 
 
 
+// funcion para obtener un numero random ya que el rgb va del 0 al 255
 const randomColor = () => {
     let r = Math.floor( Math.random() * 256 )
     let g = Math.floor( Math.random() * 256 )
     let b = Math.floor( Math.random() * 256 )
     return `rgb(${r}, ${g}, ${b})`
 }
+
+// for para crear tantos botones como se quieran
 
 for ( let i = 0; i < 25; i++ ) {
     const btn = document.createElement("button")
@@ -18,6 +21,8 @@ for ( let i = 0; i < 25; i++ ) {
 
 const btns = document.querySelectorAll("button")
 
+//  con los botonoes seleccionados, ciclo for para que cada uno llame a la funcion y pueda hacer un numero random por separado
+
 for ( let button of btns ) {
     button.addEventListener("mouseenter", () => {
         button.style.backgroundColor = randomColor();
@@ -25,6 +30,7 @@ for ( let button of btns ) {
     })
 }
 
+// lo mismo pero con h2's 
 
 for ( let i = 0; i < 10; i++ ) {
     const headers = document.createElement("h2")
