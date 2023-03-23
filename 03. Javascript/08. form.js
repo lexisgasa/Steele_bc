f49e6c928b1168171883c510034729219cd425fb
Alexis <lexisgasa3007@gmail.com>
2023-03-23 11:48:48 +0100

    comments updated

--- 03. Javascript/08. form.js
+++ 03. Javascript/08. form.js
@@ -2,6 +2,10 @@ const form = document.querySelector("form")
 const input = document.querySelector("input")
 const stuffList = document.querySelector("#stuffList")
 
+// preventdefault para no perder la data
+// crear un elemento <li> </li>, luego darle un texto interno que es el guardado en la variable del input.value
+// con el <li> </li> lleno, meterlo en el <ul></ul> y al darle al submit aparecerá en la lista
+// acabar reseteando el texto
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
