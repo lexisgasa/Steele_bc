6c69ca24aa185030e847a7417c6b36ff738d87a8
Alexis <lexisgasa3007@gmail.com>
2023-04-03 13:30:14 +0200

    axios practice

--- 03. Javascript/14.axios.js
+++ 03. Javascript/14.axios.js
@@ -21,3 +21,43 @@ const starWarsApi = async () => {
     }
 };
 starWarsApi()
+
+
+
+// primero se crea la funcion que hace el fetch de la pagina y DEVUELVE un valor
+const getDadJoke = async () => {
+    try {
+        const headers = { headers: { Accept: "application/json"}};
+        const res = await axios.get("https://icanhazdadjoke.com/", headers);
+        console.log(res.data.joke);
+        return res.data.joke;
+    } catch (error) {
+        const title = document.querySelector("h1");
+        title.innerText = "API failed"
+        return "No jokes available!"
+        console.log(error)
+    }
+}
+// getDadJoke();
+
+
+const addNewJoke = async () => {
+    try {
+        // sin el await, imprime object promise, y luego en consola el resultado, por eso con await coge el resultado y luego hace el elemento li
+        // IMPORTANTE GUARDAR EL VALOR OBTENIDO AL LLAMAR LA FUNCION PARA PODER USARLO
+        const jokeText = await getDadJoke();
+        // crear un li nuevo
+        const element = document.createElement("li");
+        // que se le añade el contenido obtenido de llamar la funcion
+        element.append(jokeText)
+        // y se le añade al elemento ul
+        list.append(element)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const btn = document.querySelector("button")
+const list = document.querySelector("ul")
+// no usar funcion() o se llamara en cuanto se recarga la pagina
+btn.addEventListener("click", addNewJoke);
