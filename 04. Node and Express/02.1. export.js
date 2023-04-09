ab473f55a0c4c24f978d807c010a1222b0290e9b
Alexis <lexisgasa3007@gmail.com>
2023-04-09 23:59:45 +0200

    export modules

---
+++ 04. Node and Express/02.1. export.js
@@ -0,0 +1,10 @@
+// documento de las mates
+
+const add = (x , y) => x + y;
+
+const PI = 3.14159
+
+const square = x => x * x;
+
+module.exports.suma = add
+module.exports.cuadrado = square
---
+++ 04. Node and Express/02.2. export.js
@@ -0,0 +1,4 @@
+const math = require("./02.1. export")
+
+console.log(math.suma(2,5))
+console.log(math.cuadrado(2))
