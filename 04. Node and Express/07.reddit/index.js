aaacc2c42b8c9bce843f9ec7749de86b77f39f7f
Alexis <lexisgasa3007@gmail.com>
2023-04-12 18:12:03 +0200

    copia reddit

--- 04. Node and Express/07.reddit/index.js
+++ 04. Node and Express/07.reddit/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+// al usar require ya se parsea automaticamente
+const redditData = require("./data.json")
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"))
@@ -10,7 +13,19 @@ app.set("views", path.join(__dirname, "/views"))
 // la respuesta a enviar es el render de la pagina home
 app.get("/r/:subreddit", (req, res) => {
     const { subreddit } = req.params;
-    res.render("home", {subreddit: subreddit})
+    // redditData tiene keys con nombres que si hay match, estaran dentro de los claudator []
+    const data = redditData[subreddit];
+    //  se usa el spread operator para tener cada valor por individual
+    if (redditData[subreddit]) {
+    res.render("home", {...data})
+    } else {
+        res.send(`<h1>Subreddit "${subreddit}" not found</h1>`)
+    }
+})
+
+app.get("/", (req, res)=> {
+    console.log("Main page!")
+    res.send("<h1>Now in the main page</h1>")
 })
 
 app.listen(3000, () => {
